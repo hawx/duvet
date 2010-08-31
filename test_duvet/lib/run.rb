34ab49a311a2ea21e547a29f91a72bebb210c28e
Joshua Hawxwell <m@hawx.me>
2010-08-31 16:56:20 +0100

    Added incomplete test

---
+++ test_duvet/lib/run.rb
@@ -0,0 +1,21 @@
+class Run
+  
+  def run_it
+    run('it')
+  end
+  
+  def run_here
+    run('here')
+  end
+  
+  def run_there
+    run('there')
+  end
+
+  
+  def run(word)
+    return "run #{word}"
+    "Hey guess what I will never ever run."
+  end
+
+end
---
+++ test_duvet/test/test_all.rb
@@ -0,0 +1,59 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', '..', 'lib'))
+require 'duvet'
+Duvet.start({:dir => 'test_duvet/cov', :filter => 'test_duvet/'})
+
+
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'klass'
+require 'run'
+
+class Test::Unit::TestCase
+end
+
+# This should run every line
+class TestKlass < Test::Unit::TestCase
+  
+  should "start out ok" do
+    k = Klass.new
+    assert_equal "EVERYTHING IS OK!", k.shout!
+  end
+  
+  should "need help when alerted" do
+    k = Klass.new
+    k.alert!
+    assert_equal true, k.help?
+  end
+
+  should "now shout for help" do
+    k = Klass.new
+    k.alert!
+    assert_equal "I NEED HELP!", k.shout!
+  end
+    
+end
+
+# This one should not run every line
+class TestRun < Test::Unit::TestCase
+
+  # should "run it" do
+  #   me = Run.new
+  #   assert_equal "run it", me.run_it
+  # end
+  
+  # should "run here" do
+  #   me = Run.new
+  #   assert_equal "run here", me.run_here
+  # end
+  
+  should "run there" do
+    me = Run.new
+    assert_equal "run there", me.run_there
+  end
+
+end
